cd3479020a6b675f802a9af96d4ad185db5504e6
Ahmad <ahmadamakxy128@gmail.com>
2023-08-13 02:53:24 -0700

    all

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,6 @@ if __name__ == "__main__":
     argv = sys.argv[1:]
     num_arguments = len(argv)
 
-    print("{} argument{}:".format(num_arguments,
-        's' if num_arguments != 1 else ''))
+    print(f"{num_arguments} argument{'s' if num_arguments != 1 else ''}:")
     for i, arg in enumerate(argv, start=1):
-            print("{}: {}".format(i, arg))
-            
+        print(f"{i}: {arg}")
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,8 +4,11 @@ if __name__ == "__main__":
     import sys
     import hidden_4
     module_names = dir(hidden_4)
-    filtered_names = sorted(name for name in module_names if not name.startswith("__"))
+    filtered_names = sorted(
+            name
+            for name in module_names
+            if not name.startswith("__")
+            )
     for name in filtered_names:
         for name in filtered_names:
             print("{}".format(name))
-
