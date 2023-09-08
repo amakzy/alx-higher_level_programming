29ce366cd8aa04dfb5a8bfb3131b16a5afca067c
amakzy <ahmadamakxy128@gmail.com>
2023-09-08 23:41:51 +0000

    a

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -8,9 +8,9 @@ def matrix_mul(m_a, m_b):
         m_a (list of lists): first matrix
         m_b (list of lists): second matrix
     """
-    if type(m_a) != list:
+    if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
-    if type(m_b) != list:
+    if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
 
     # variables to verify if both m_a and m_b can be multiplied
@@ -21,7 +21,7 @@ def matrix_mul(m_a, m_b):
     if m_a == []:
         raise ValueError("m_a can't be empty")
     for row1 in m_a:
-        if type(row1) != list:
+        if not isinstance(row1, list):
             raise TypeError("m_a must be a list of lists")
         len1 = len(m_a[0])
         if row1 == []:
@@ -30,14 +30,14 @@ def matrix_mul(m_a, m_b):
             raise TypeError("each row of m_a must be of the same size")
         num_colum1 = len(row1)
         for column1 in row1:
-            if type(column1) != int and type(column1) != float:
+            if not isinstance(column1, (int, float)):
                 raise TypeError("m_a should contain only integers or floats")
 
     # Check requirements for matrix m_b
     if m_b == []:
         raise ValueError("m_b can't be empty")
     for row2 in m_b:
-        if type(row2) != list:
+        if not isinstance(row2, list):
             raise TypeError("m_b must be a list of lists")
         len2 = len(m_b[0])
         if row2 == []:
@@ -46,7 +46,7 @@ def matrix_mul(m_a, m_b):
             raise TypeError("each row of m_b must be of the same size")
         num_row2 += 1
         for column2 in row2:
-            if type(column2) != int and type(column2) != float:
+            if not isinstance(column2, (int, float)):
                 raise TypeError("m_b should contain only integers or floats")
 
     # Check if the multiplication is posible
@@ -56,16 +56,16 @@ def matrix_mul(m_a, m_b):
     mul_matrix = []
 
     for row_1 in m_a:
-        l = 0
+        row_index = 0
         l_row = []
-        while l < len(m_b[0]):
+        while row_index < len(m_b[0]):
             result = 0
             k = 0
             for column_1 in row_1:
-                result += column_1 * m_b[k][l]
+                result += column_1 * m_b[k][row_index]
                 k += 1
             l_row.append(result)
-            l += 1
+            row_index += 1
         mul_matrix.append(l_row)
 
     return mul_matrix
