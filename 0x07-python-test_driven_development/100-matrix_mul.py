0dd7c2b58e146c3e967434668f3c57b579af4ff8
amakzy <ahmadamakxy128@gmail.com>
2023-09-09 00:10:16 +0000

    a

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,42 +1,42 @@
 #!/usr/bin/python3
 def matrix_mul(m_a, m_b):
 
-    # Validate input
+    # Validate m_a
+
     if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
-    if not isinstance(m_b, list):
-        raise TypeError("m_b must be a list")
-
     if not all(isinstance(row, list) for row in m_a):
         raise TypeError("m_a must be a list of lists")
+    if len(m_a) == 0:
+        raise ValueError("m_a can't be empty")
+    if any(not all(isinstance(num, (int, float)) for num in row) for row in m_a[:-1]):
+    raise TypeError("m_a should contain only integers or floats")
+
+
+if not all(isinstance(num, (int, float)) for num in m_a[-1]):
+    raise TypeError("m_a should contain only integers or floats")
+
+    # Validate m_b
+
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
     if not all(isinstance(row, list) for row in m_b):
         raise TypeError("m_b must be a list of lists")
-
-    if len(m_a) == 0 or len(m_b) == 0:
-        raise ValueError("m_a and m_b can't be empty")
-
-    if not all(all(isinstance(x, (int, float)) for x in row) for row in m_a):
-        raise TypeError("m_a should contain only integers or floats")
-    if not all(all(isinstance(x, (int, float)) for x in row) for row in m_b):
-        raise TypeError("m_b should contain only integers or floats")
-
-    if not all(len(row) == len(m_a[0]) for row in m_a):
-        raise TypeError("each row of m_a must be of the same size")
-    if not all(len(row) == len(m_b[0]) for row in m_b):
-        raise TypeError("each row of m_b must be of the same size")
-
+    if len(m_b) == 0:
+        raise ValueError("m_b can't be empty")
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
+    if any(not all(lambda num: isinstance(num, (int, float)), row) for row in m_b):
+        raise TypeError("m_b should contain only integers or floats")
 
-    # Calculate product
-    product = []
+    # Calculate the product of the matrices
+
+    result = []
     for i in range(len(m_a)):
         row = []
         for j in range(len(m_b[0])):
-            sum = 0
-            for k in range(len(m_b)):
-                sum += m_a[i][k] * m_b[k][j]
-            row.append(sum)
-        product.append(row)
+            elem = sum(m_a[i][k] * m_b[k][j] for k in range(len(m_b)))
+            row.append(elem)
+        result.append(row)
 
-    return product
+    return result
