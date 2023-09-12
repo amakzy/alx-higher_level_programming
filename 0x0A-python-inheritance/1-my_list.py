1ba343a7b7d1cdef5c869452ff30fb0b3bff5f5b
amakzy <ahmadamakxy128@gmail.com>
2023-09-12 04:20:56 +0300

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,18 +1,15 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 """
-Module: 1-my_list
-
-Description:
-    Defines the MyList class that inherits the list.
-
-    Public Methods:
-        - print_sorted(self): Prints the list, but sorted in ascending order.
+contains the MyList class
 """
+
 
 class MyList(list):
-    """Inherits my list"""
+    """a subclass of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
 
     def print_sorted(self):
-        """Prints the list, but sorted (ascending sort)"""
-        sorted_list = sorted(self)
-        print(sorted_list)
+        """prints the sorted list"""
+        print(sorted(self))
